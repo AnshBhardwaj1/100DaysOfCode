fd9995cbdd990677e02a65a5c4798a77063c5eb3
AnshBhardwaj <AnshBhardwaj119@gmail.com>
2023-03-15 03:08:13 +0530

    time ka programme bana diya

--- second.py
+++ second.py
@@ -1,7 +1,11 @@
 num1=int(input("num 1 = "))
 num2=int(input("num 2 = "))
+"hrllo"
 print ("Sum is : ",num1+num2)
 print ("Difference is : ",num1-num2)
 print ("Product is : ",num1*num2)
 print ("Dividend is : ",num1/num2)
 num2=int(input())
+apple="hello mera naam ansh hai aur mainDTU ka student hu"
+for dumdum in apple :
+    print (dumdum)
---
+++ time.py
@@ -0,0 +1,15 @@
+import time
+hour=int(time.strftime('%H'))
+print ("hour is ",hour)
+minute=int(time.strftime('%M'))
+print ("minute is ",minute)
+sec=int(time.strftime('%S'))
+print ("seconds  is ",sec)
+if (hour <12 ):
+    print("Good morning")
+elif (hour <4 and hour > 12):
+    print ("good afternoon")
+elif (hour <9 and hour >5):
+    print ("good evening")
+else :
+    print ("good night")
